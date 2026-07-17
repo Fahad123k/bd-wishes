4ad5e9c316a2fe1a05ede7cff50b455e2931e451
fahad123k <ahmad.nishu08@gmail.com>
2026-07-17 22:10:52 +0530

    direct use router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import AppRouter from "./Router/AppRouter";
+import AppRouter from "./AppRouter";
 
 function App() {
   return <AppRouter />;
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,12 +3,12 @@ import {
     RouterProvider,
 } from "react-router-dom";
 
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import NotFound from "../pages/NotFound";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
     {
